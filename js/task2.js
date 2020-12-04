852eda371daaa85adc435ce15e2e03623410d323
supert111 <31413087+supert111@users.noreply.github.com>
2020-12-04 16:18:51 +0200

    Add files via upload

--- js/task2.js
+++ js/task2.js
@@ -1,5 +1,29 @@
-const total = 100;
-const order = 20;
+const calculateEngravingPrice = function(message, pricePerWord) {
+    const result = message.split(' ').length * pricePerWord;
+    return result;
+};
 
-const userOrder = total >= order ? 'Заказ оформлен, с вами свяжется менеджер' : 'На складе недостаточно товаров!';
-console.log(userOrder);
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
--- js/task3.js
+++ js/task3.js
@@ -1,12 +1,22 @@
-const ADMIN_PASSWORD = 'jqueryismyjam';
+// Напиши фукцнию findLongestWord(string), 
+// которая принимает параметром произвольную строку 
+// (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+// let shortWord = 1;
+const findLongestWord = function(string) {
+  // твой код
+  if (string !== Number) {
+      return;
+  }
+  for (i = 0; i < string.splite(' ').length; i++) {
+      console.log(string);
+  }
+};
 
-let message = prompt('Пожалуйста введите пароль');
-if (message === null) {
-    alert('Отменено пользователем!');
-}
-else if (message === ADMIN_PASSWORD) {
-    alert('Добро пожаловать!');
-}
-else {
-    alert('Доступ запрещен, неверный пароль!');
-}
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
