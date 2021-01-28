658b2158882f3142b0dc5edf682fe292d4a6b2e9
supert111 <31413087+supert111@users.noreply.github.com>
2021-01-29 01:56:25 +0200

    Add files via upload

--- js/task1.js
+++ js/task1.js
@@ -1,2 +1,58 @@
-import users from "./gallery-items.js"
+import galleryItems from "./gallery-items.js"
 
+const jsGalleryRef = document.querySelector('.js-gallery');
+
+
+
+const liMarkup = (element => {
+    const galleryItemRef = document.createElement('li');
+    const gallerylinkRef = document.createElement('a');
+    const galleryImageRef = document.createElement('img');
+    
+    galleryItemRef.classList.add('gallery__item');
+    gallerylinkRef.classList.add('gallery__link');
+    galleryImageRef.classList.add('gallery__image');
+    
+    gallerylinkRef.setAttribute('href', 'element.original');
+    galleryImageRef.setAttribute('src', 'element.preview');
+    galleryImageRef.setAttribute('data-source','element.original');
+    galleryImageRef.setAttribute('alt', 'element.description');
+
+    galleryItemRef.append(gallerylinkRef);
+    galleryItemRef.append(galleryImageRef);
+    
+    return galleryItemRef;
+});
+console.dir(galleryItems);
+const allGallery = galleryItems.map(element =>{
+   // console.log(element[key]);
+  //liMarkup(element);
+
+ //console.dir(Object.values(elements));
+    // .forEach(element => {
+    //     return liMarkup(Object.values(element));
+    // });
+    //console.dir(element[key]);
+    //  liMarkup(element);
+     
+    return;
+    //console.dir(liMarkup);
+})
+console.dir(allGallery);
+  jsGalleryRef.append(...allGallery);
+  console.dir(jsGalleryRef);
+
+//console.dir(jsGalleryRef);
+// <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//       alt="Tulips"
+//     />
+//   </a>
+// </li>
