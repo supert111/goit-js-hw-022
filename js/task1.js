33bb870555b7f701e6c62e01f9dd243e91450d09
supert111 <31413087+supert111@users.noreply.github.com>
2021-01-29 16:12:55 +0200

    Add files via upload

--- js/task1.js
+++ js/task1.js
@@ -2,9 +2,7 @@ import galleryItems from "./gallery-items.js"
 
 const jsGalleryRef = document.querySelector('.js-gallery');
 
-
-
-const liMarkup = (element => {
+const liMarkup = (el => {
     const galleryItemRef = document.createElement('li');
     const gallerylinkRef = document.createElement('a');
     const galleryImageRef = document.createElement('img');
@@ -13,36 +11,24 @@ const liMarkup = (element => {
     gallerylinkRef.classList.add('gallery__link');
     galleryImageRef.classList.add('gallery__image');
     
-    gallerylinkRef.setAttribute('href', 'element.original');
-    galleryImageRef.setAttribute('src', 'element.preview');
-    galleryImageRef.setAttribute('data-source','element.original');
-    galleryImageRef.setAttribute('alt', 'element.description');
-
+    gallerylinkRef.setAttribute('href', el.original);
+    galleryImageRef.setAttribute('src', el.preview);
+    galleryImageRef.setAttribute('data-source', el.original);
+    galleryImageRef.setAttribute('alt', el.description);
     galleryItemRef.append(gallerylinkRef);
     galleryItemRef.append(galleryImageRef);
     
     return galleryItemRef;
 });
-console.dir(galleryItems);
-const allGallery = galleryItems.map(element =>{
-   // console.log(element[key]);
-  //liMarkup(element);
 
- //console.dir(Object.values(elements));
-    // .forEach(element => {
-    //     return liMarkup(Object.values(element));
-    // });
-    //console.dir(element[key]);
-    //  liMarkup(element);
-     
-    return;
-    //console.dir(liMarkup);
-})
-console.dir(allGallery);
-  jsGalleryRef.append(...allGallery);
-  console.dir(jsGalleryRef);
+const allGallery = galleryItems.map(element => {
+    return liMarkup(element);
+    
+});
+   jsGalleryRef.append(...allGallery);
 
-//console.dir(jsGalleryRef);
+
+
 // <li class="gallery__item">
 //   <a
 //     class="gallery__link"
